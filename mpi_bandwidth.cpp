add6fe6ce29394282a98fcbbfb8d63c3e8eb3d62
Tobias Rohner <rohner.tobias@gmail.com>
2022-12-20 19:04:13 +0100

    Code to benchmark custom scheduling on Piz Daint

---
+++ mpi_bandwidth.cpp
@@ -0,0 +1,96 @@
+#include <mpi.h>
+#include <iostream>
+#include <memory>
+#include <chrono>
+#include <vector>
+#include <algorithm>
+
+
+
+std::vector<int> comm_schedule(MPI_Comm comm) {
+  int rank, size;
+  MPI_Comm_rank(comm, &rank);
+  MPI_Comm_size(comm, &size);
+  std::vector<int> msgs;
+  for (int r = 0; r < size; ++r) {
+    msgs.push_back((r + rank) % size);
+  }
+  std::vector<int> schedule(size);
+  for (int r = 0; r < size; ++r) {
+    schedule[msgs[r]] = r;
+  }
+  return schedule;
+}
+
+
+
+
+
+int main(int argc, char *argv[]) {
+  MPI_Init(&argc, &argv);
+  int size, rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  // Each rank schould send up to 1GB of data to the other ranks
+  static constexpr size_t KB = 1024;
+  static constexpr size_t MB = 1024 * 1024;
+  static constexpr size_t GB = 1024 * 1024 * 1024;
+  const size_t max_msg_size = std::min(64*GB / size, 2*GB);
+  for (size_t msg_size = KB ; msg_size < max_msg_size; msg_size <<= 1) {
+    if (rank == 0) {
+      std::cout << "\nMessage Size: ";
+      if (msg_size < MB) {
+	std::cout << msg_size/KB << "KB";
+      } else if (msg_size < GB) {
+	std::cout << msg_size/MB << "MB";
+      } else {
+	std::cout << msg_size/GB << "GB";
+      }
+      std::cout << std::endl;
+    }
+    size_t num_bytes = size * msg_size;
+    auto senddata = std::make_unique<char[]>(num_bytes);
+    auto recvdata = std::make_unique<char[]>(num_bytes);
+
+    // Measure the performance of MPI_Alltoall
+    std::vector<double> speed_alltoall;
+    for (int i = 0 ; i < 100 ; ++i) {
+      MPI_Barrier(MPI_COMM_WORLD);
+      const auto start = std::chrono::steady_clock::now();
+      MPI_Alltoall(senddata.get(), msg_size, MPI_CHAR, recvdata.get(), msg_size, MPI_CHAR, MPI_COMM_WORLD);
+      MPI_Barrier(MPI_COMM_WORLD);
+      const auto end = std::chrono::steady_clock::now();
+      const double elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
+      speed_alltoall.push_back(static_cast<double>(num_bytes)/GB/elapsed);
+    }
+    std::sort(speed_alltoall.begin(), speed_alltoall.end());
+    if (rank == 0) {
+      std::cout << "MPI_Alltoall Max Speed: " << speed_alltoall.back() << "GB/s" << std::endl;
+    }
+
+    // Measure the performance of doing an alltoall with a series of MPI_Sendrecv
+    const std::vector<int> schedule = comm_schedule(MPI_COMM_WORLD);
+    std::vector<double> speed_sendrecv;
+    for (int i = 0 ; i < 100 ; ++i) {
+      MPI_Barrier(MPI_COMM_WORLD);
+      const auto start = std::chrono::steady_clock::now();
+      for (size_t r: schedule) {
+	char *senddata_start = senddata.get() + r * msg_size;
+	char *recvdata_start = recvdata.get() + r * msg_size;
+	MPI_Sendrecv(senddata_start, msg_size, MPI_CHAR, r, 0, recvdata_start, msg_size, MPI_CHAR, r, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      }
+      MPI_Barrier(MPI_COMM_WORLD);
+      const auto end = std::chrono::steady_clock::now();
+      const double elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
+      speed_sendrecv.push_back(static_cast<double>(num_bytes)/GB/elapsed);
+    }
+    std::sort(speed_sendrecv.begin(), speed_sendrecv.end());
+    if (rank == 0) {
+      std::cout << "MPI_Sendrecv Max Speed: " << speed_sendrecv.back() << "GB/s" << std::endl;
+    }
+  }
+
+  MPI_Finalize();
+  return 0;
+}
---
+++ plot_results.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+from functools import reduce
+
+import matplotlib as mpl
+#mpl.rcParams['figure.dpi'] = 300
+
+
+TMPDIR = sys.argv[1]
+OUTDIR = sys.argv[2]
+
+
+file_re = re.compile(r'^slurm-mpi_\d+\.\d{8}\.out$')
+files = [f for f in os.listdir(TMPDIR) if file_re.match(f) is not None]
+print(files)
+
+ranks = list(sorted(list(set([int(f.split('.')[0][10:]) for f in files]))))
+print(ranks)
+
+datafiles = [(rank, max(filter(lambda f: int(f.split('_')[1].split('.')[0]) == rank,
+                               files
+                              ),
+                        key=lambda f: int(f.split('.')[1])
+                       )) for rank in ranks]
+print(datafiles)
+
+bandwidth_cray = {}
+bandwidth_custom = {}
+for rank, fname in sorted(datafiles, key=lambda f: f[0]):
+    if rank not in bandwidth_cray:
+        bandwidth_cray[rank] = {}
+    if rank not in bandwidth_custom:
+        bandwidth_custom[rank] = {}
+    with open(os.path.join(TMPDIR, fname), 'r') as f:
+        msg_size = None
+        for line in f.readlines():
+            if line.startswith('Message Size:'):
+                msg_size = line.split(' ')[-1].strip()
+            if line.startswith('MPI_Alltoall'):
+                bandwidth_cray[rank][msg_size] = float(line.split(' ')[-1][:-5])
+            if line.startswith('MPI_Sendrecv'):
+                bandwidth_custom[rank][msg_size] = float(line.split(' ')[-1][:-5])
+print(bandwidth_cray)
+print(bandwidth_custom)
+
+msg_sizes = list(sorted(list(reduce(lambda a,b: a|b, [set(bandwidth_cray[rank].keys()) for rank in ranks], set())
+                             | reduce(lambda a,b: a|b, [set(bandwidth_custom[rank].keys()) for rank in ranks], set())),
+                        key = lambda size: int(size[:-2]) * {'KB':1024, 'MB':1024**2, 'GB':1024**3}[size[-2:]]))
+print(msg_sizes)
+
+num_ranks = len(ranks)
+num_msg_sizes = len(msg_sizes)
+bw_cray_arr = np.full((num_ranks, num_msg_sizes), np.nan)
+bw_custom_arr = np.full((num_ranks, num_msg_sizes), np.nan)
+for ridx, rank in enumerate(ranks):
+    for msidx, msg_size in enumerate(msg_sizes):
+        bw_cray_arr[ridx,msidx] = bandwidth_cray[rank].get(msg_size, np.nan)
+        bw_custom_arr[ridx,msidx] = bandwidth_custom[rank].get(msg_size, np.nan)
+
+def plot(data, title, **kwargs):
+    plt.pcolormesh(data, **kwargs)
+    plt.colorbar()
+    plt.xticks(np.linspace(0.5, num_msg_sizes-0.5, num_msg_sizes), msg_sizes)
+    plt.yticks(np.linspace(0.5, num_ranks-0.5, num_ranks), ranks)
+    plt.xlabel('Message Size')
+    plt.ylabel('Number of MPI Ranks')
+    plt.title(title)
+
+plot(bw_cray_arr, 'Bandwidth of Cray MPI_Alltoall in GB/s')
+plt.show()
+plot(bw_custom_arr, 'Bandwidth of Custom MPI_Alltoall in GB/s')
+plt.show()
+speedup = bw_custom_arr / bw_cray_arr
+max_diff = max(np.nanmax(speedup), 1/np.nanmin(speedup))
+print(max_diff)
+plot(bw_custom_arr / bw_cray_arr, 'Speedup of Custom MPI_Alltoall over Cray', cmap='RdBu', norm=mpl.colors.LogNorm(vmin=1/max_diff, vmax=max_diff))
+plt.show()
