30885f317a97eeda4a9eb8415ab82b06a0e24913
Tobias Rohner <torohner@eu-login-20.euler.ethz.ch>
2022-12-21 09:36:11 +0100

    Submission scripts for euler

---
+++ euler/submit_8.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#SBATCH --job-name=mpi_8
+#SBATCH --time=00:40:00
+#SBATCH --ntasks=8
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=65536MB
+#SBATCH --hint=nomultithread
+#SBATCH --output=slurm-%x.%j.out
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+mpirun mpi_bandwidth
