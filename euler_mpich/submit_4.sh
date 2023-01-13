e8d9ffb150c0a40effea9b0be6bd3523c2ab9d18
Tobias Rohner <torohner@eu-login-29.euler.ethz.ch>
2023-01-13 14:53:25 +0100

    Run mpich on Euler

---
+++ euler_mpich/submit_4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#SBATCH --job-name=mpi_4
+#SBATCH --time=00:20:00
+#SBATCH --ntasks=4
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=65536MB
+#SBATCH --hint=nomultithread
+#SBATCH --output=slurm-%x.%j.out
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+~/mpich/bin/mpirun ../mpi_bandwidth
