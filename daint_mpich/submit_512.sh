535b427a6dd0b15d0f974306dc03f027a109650a
Tobias Rohner <tobias.rohner@math.ethz.ch>
2023-01-15 16:28:51 +0100

    Run daint mpich up to 1024 nodes

---
+++ daint_mpich/submit_512.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH --job-name=mpi_512
+#SBATCH --account="s1069"
+#SBATCH --time=01:00:00
+#SBATCH --nodes=512
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --partition=normal
+#SBATCH --constraint=gpu
+#SBATCH --hint=nomultithread
+#SBATCH --output=slurm-%x.%j.out
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+srun ../mpi_bandwidth
